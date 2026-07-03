504c77c58548e7691e69ca5fefe039038767a7e7
dpcivl <dpcivl713@gmail.com>
2026-07-03 15:13:42 +0900

    Week1 Day1: 자바 첫 실행, 변수 기초

---
+++ week1-basics/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "코드런";
+        int age = 3; 
+        double height = 175.5;
+
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("키: " + height);
+
+        int nextYear = age + 1;
+        System.out.println("내년 나이: " + nextYear);
+    }
+}
